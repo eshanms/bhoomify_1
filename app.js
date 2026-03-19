160f860cfb1d8d89852ed591a1df9ce9a77e79bc
eshanms <eshanms2007@gmail.com>
2026-03-19 21:14:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:5000"; // change after deployment
+//const API = "http://localhost:5000"; // change after deployment
 
 function login(e) {
   e.preventDefault();
@@ -11,16 +11,18 @@ function login(e) {
     window.location.href = "faculty.html";
   }
 }
-
+const leaderboardData = [
+  { name: "Eshan", points: 120 },
+  { name: "Aneya", points: 95 },
+  { name: "Arathy", points: 80 },
+  { name: "Gokul", points: 70 }
+];
 // Load leaderboard
-async function loadLeaderboard() {
-  let res = await fetch(API + "/leaderboard");
-  let data = await res.json();
-
+function loadLeaderboard() {
   let list = document.getElementById("leaderboard");
   list.innerHTML = "";
 
-  data.forEach(user => {
+  leaderboardData.forEach(user => {
     let li = document.createElement("li");
     li.innerText = user.name + " - " + user.points;
     list.appendChild(li);
@@ -28,20 +30,33 @@ async function loadLeaderboard() {
 }
 
 // Assign points
-async function assignPoints(e) {
+function assignPoints(e) {
   e.preventDefault();
-
+  loadData();
   let name = document.getElementById("studentName").value;
-  let points = document.getElementById("points").value;
+  let points = parseInt(document.getElementById("points").value);
 
-  await fetch(API + "/assign-points", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ name, points })
-  });
+  // Find student
+  let student = leaderboardData.find(u => u.name === name);
+
+  if (student) {
+    student.points += points;
+  } else {
+    leaderboardData.push({ name, points });
+  }
 
   alert("Points Assigned!");
+  saveData();
+}
+// Save data
+function saveData() {
+  localStorage.setItem("leaderboard", JSON.stringify(leaderboardData));
 }
 
+// Load data
+function loadData() {
+  let data = localStorage.getItem("leaderboard");
+  if (data) {
+    leaderboardData.splice(0, leaderboardData.length, ...JSON.parse(data));
+  }
+}
