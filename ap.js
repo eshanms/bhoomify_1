2bc971a2c9318071cbabcdda460c6c40f7c4d966
eshanms <eshanms2007@gmail.com>
2026-03-20 00:44:47 +0530

    Add files via upload

---
+++ ap.js
@@ -0,0 +1,16 @@
+
+let users = [
+  { name: "Eshan", password: "123", role: "student" },
+  { name: "Faculty1", password: "admin", role: "faculty" }
+];
+
+function login() {
+  let name = document.getElementById("username").value;
+  let pass = document.getElementById("password").value;
+
+  let user = users.find(u => u.name === name && u.password === pass);
+
+  if (!user) {
+    alert("Invalid login!");
+    return;
+  }
